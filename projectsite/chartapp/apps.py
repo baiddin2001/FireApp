155b240fd17343f0aa81e493adec6877d9b61d71
kylapot <kylapot>
2024-05-02 21:14:47 +0800

    add all

--- projectsite/chartapp/apps.py
+++ projectsite/chartapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ChartappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'chartapp'
+class FireConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "fire"
