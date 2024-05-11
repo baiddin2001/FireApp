84dea2de98738ff6c0cdd3cd5bdf5f8425e2895f
baiddin2001 <201980350@palawan.edu.ph>
2024-05-11 14:20:57 +0800

    modify FireApp

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -1,18 +1,21 @@
 from django.contrib import admin
 from django.urls import path
 
-from fire.views import HomePageView
-from fire.views import HomePageView, ChartView, PieCountbySeverity, LineCountbyMonth, MultilineIncidentTop3Country, multipleBarbySeverity
+from fire.views import HomePageView, ChartView, PieCountbySeverity, LineCountbyMonth, MultilineIncidentTop3Country, multipleBarbySeverity, fire_incidents_map, delete_location
 from fire import views
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', HomePageView.as_view(), name='home'),
     path('dashboard_chart', ChartView.as_view(), name='dashboard-chart'),
-    path('chart/', PieCountbySeverity, name='chart'),
+    path('PieChart/', PieCountbySeverity, name='chart'),
     path('lineChart/', LineCountbyMonth, name='chart'),
     path('multilineChart/', MultilineIncidentTop3Country, name='chart'),
     path('multiBarChart/', multipleBarbySeverity, name='chart'),
     path('stations', views.map_station, name='map-station'),
+    path('fire_incidents_map/', views.fire_incidents_map, name='fire_incidents_map'),
+    path('database/', views.database_view, name='database'),
+    path('delete/<int:location_id>/', delete_location, name='delete_location'),
 
 ]
